82defefba444fd40bdd8cb4816fb49a1e80f7f5d
Mrmagicpie <realmrmagicpie@gmail.com>
2021-01-25 23:25:06 -0500

    lmao

--- Shell.py
+++ Shell.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 import os, asyncio, sys
 
-class ReloadItHoe(Exception):
-    pass
-class GetMeOutOfHereBitch(Exception):
-    pass
+class ReloadItHoe(Exception): pass
+class GetMeOutOfHereBitch(Exception): pass
 
 class shell:
     def __init__(self):
         self.version = "0.1"
         self.SHELL = "change me if you want idc"
+        self.reloaded = 0
 
     async def command_handler(self, command = None):
         if command is None or command == "\n" or command == " ":
@@ -29,8 +28,14 @@ class shell:
     async def main_shell(self):
         while True:
             try:
+                try:
+                    if sys.argv[1] == "reloadedhoelmao" and self.reloaded == 0:
+                        self.reloaded = 1
+                        print("\nReloaded!\n")
+                except:
+                    pass
                 command = input(self.SHELL + ": $ ")
-                if command.lower() == "exit":
+                if command.lower() == "exit" or command.lower() == "exit()":
                     print("\nYou think it's that easy? Lmfao gj\n")
                 else:
                     await self.command_handler(command=command)
@@ -47,7 +52,7 @@ if __name__ == "__main__":
         shell = shell()
         asyncio.get_event_loop().run_until_complete(shell.main_shell())
     except ReloadItHoe:
-        print("\nReloaded!\n")
-        os.execv(__file__, sys.argv)
+        # print("\nReloaded!\n")
+        os.execv(__file__, [sys.argv[0], "reloadedhoelmao"])
     except GetMeOutOfHereBitch:
         print("\nFine you're free. smfh\n")
